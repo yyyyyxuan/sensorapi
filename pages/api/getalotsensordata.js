9bf8b817defaf733b7a4f8db04feb43bccc36f8f
yyyyyxuan <2103463B@student.tp.edu.sg>
2023-06-28 13:48:26 +0800

    sg time|1

--- pages/api/getalotsensordata.js
+++ pages/api/getalotsensordata.js
@@ -19,7 +19,7 @@ export default function handler(req, res) {
     }
 
     // Execute the query to retrieve the data column from the sensordata table
-    const query = 'SELECT * FROM ( SELECT * FROM sensordata ORDER BY id DESC LIMIT 10 ) AS subquery ORDER BY id ASC;';
+    const query = 'SELECT * FROM ( SELECT DATE_ADD(time, INTERVAL 8 HOUR) AS converted_time, data, time, id FROM sensordata ORDER BY id DESC LIMIT 10 ) AS subquery ORDER BY id ASC;';
     connection.query(query, (error, results) => {
       if (error) {
         console.error('Error retrieving data:', error);
