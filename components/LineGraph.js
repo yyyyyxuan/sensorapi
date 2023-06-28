10b5cf97dfcb81633d056525e556d007128378e7
yyyyyxuan <2103463B@student.tp.edu.sg>
2023-06-28 15:38:29 +0800

    added zoom | fix width for graph

--- components/LineGraph.js
+++ components/LineGraph.js
@@ -1,48 +1,73 @@
-"use client";
+"use client"
+import { useEffect } from 'react';
+import dynamic from 'next/dynamic';
 import { Chart as ChartJS, registerables } from 'chart.js';
 import { Line } from 'react-chartjs-2';
 
 ChartJS.register(...registerables);
 
-export default function LineGraph(props) {
+const LineGraph = (props) => {
+  useEffect(() => {
+    import('chartjs-plugin-zoom').then((ZoomPlugin) => {
+      ChartJS.register(ZoomPlugin.default);
+    });
+  }, []);
 
-    const chartData = {
-        labels: props.time,
-        datasets: [
-          {
-            label: props.label,
-            data: props.data,
-            fill: false,
-            borderColor: 'rgb(75, 160, 255)',
-          },
-        ],
-      };
-      const options = {
-        scales: {
-          y: {
-            suggestedMin: Math.min(...props.data), // Adjust as needed
-            suggestedMax: Math.max(...props.data),
-            beginAtZero: false,
-            ticks: {
-              maxTicksLimit: props.maxTicksLimit,
-              callback: function (value, index, values) {
-                return value.toFixed(props.dp) + props.unit; 
-              },
-            },
-          },
-          x: {
-            ticks: {
-              maxTicksLimit: 5 // Adjust the number of desired intermediate ticks
+  const chartData = {
+    labels: props.time,
+    datasets: [
+      {
+        label: props.label,
+        data: props.data,
+        fill: false,
+        borderColor: 'rgb(75, 160, 255)',
+      },
+    ],  
+  };
 
-            },
+  const options = {
+    scales: {
+      y: {
+        suggestedMin: Math.min(...props.data),
+        suggestedMax: Math.max(...props.data),
+        beginAtZero: false,
+        ticks: {
+          maxTicksLimit: props.maxTicksLimit,
+          callback: function (value, index, values) {
+            return value.toFixed(props.dp) + props.unit;
           },
         },
-      };
-    return (
-        <div>
-            <Line data={chartData} options={options}/>
-        </div>
+      },
+      x: {
+        ticks: {
+          maxTicksLimit: 5,
+        },
+      },
+    },
+    plugins: {
+      zoom: {
+        pan: {
+          enabled: true,
+          mode: 'xy',
+        },
+        zoom: {
+          wheel: {
+            enabled: true,
+          },
+          pinch: {
+            enabled: true,
+          },
+          mode: 'xy',
+        },
+      },
+    },
+  };
 
-    );
+  return (
+    <div>
+      <Line data={chartData} options={options} />
+    </div>
+  );
+};
 
-}
+export default LineGraph;
