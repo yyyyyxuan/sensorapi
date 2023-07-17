15935946ca648e50684792398427908731c032a9
yyyyyxuan <2103463B@student.tp.edu.sg>
2023-07-17 11:15:18 +0800

    query

--- components/Graph.tsx
+++ components/Graph.tsx
@@ -69,7 +69,7 @@ const Graph: React.FC<GraphProps> = ({ jsonData }) => {
   
   const temp = data.map((item) => item.data.temp);
   const time = data.map((item) => {
-    const dateTime = dayjs(item.time).utcOffset(8).format('DD/MM/YYYY, h:mm:ss A');
+    const dateTime = dayjs(item.time).format('DD/MM/YYYY, h:mm:ss A');
     return dateTime;
   });
   const PM1 = data.map((item) => item.data.PM1);
